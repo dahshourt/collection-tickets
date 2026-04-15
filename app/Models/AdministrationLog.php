e3e516ad5dc086997bbc110441f287782745865a
dahshourt <42882017+dahshourt@users.noreply.github.com>
2026-04-15 23:07:33 +0200

    Update AdministrationLog.php

--- app/Models/AdministrationLog.php
+++ app/Models/AdministrationLog.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Model;
 
-class AdministrationLog extends BaseModel
+
+class AdministrationLog extends Model
 {
     protected $fillable = [
         'user_id',
